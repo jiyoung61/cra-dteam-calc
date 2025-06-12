489ef623e7d364180a8be92c892af941d840f556
chiwon.park <chiwona123@gmail.com>
2025-06-12 10:13:33 +0900

    1. get_nzegop method 추가 2. test_get_nzegop unit test 추가

--- calc.py
+++ calc.py
@@ -1,2 +1,6 @@
 class Calc:
+
+
+    def get_nzegop(self, a:int, n:int):
+        return a**n
     pass
--- test_calc.py
+++ test_calc.py
@@ -1,4 +1,13 @@
 import pytest
+from calc import Calc
 
 def test_sample():
     pass
+
+def test_get_nzegop():
+    result1 = Calc().get_nzegop(2, 3)
+    result2 = Calc().get_nzegop(3, 2)
+    result3 = Calc().get_nzegop(4, 3)
+    assert result1 == 8
+    assert result2 == 9
+    assert result3 == 64
